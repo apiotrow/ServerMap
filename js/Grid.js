5ed6835f793fbe4a0b2a61f534c485ee42de4bc9
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2017-10-04 03:18:25 -0700

    square rendering can be done as a single graphics object, or with individual graphics object for each square

---
+++ js/Grid.js
@@ -0,0 +1,120 @@
+
+class Grid{
+	constructor(app, gridW, gridH){
+		const separate = true
+
+		let gridGraphics
+		if(!separate){
+			gridGraphics = new PIXI.Graphics()
+			gridGraphics.beginFill(0xFF3300)
+			app.stage.addChild(gridGraphics)
+		}
+
+		let gridD = 100
+		let gridSquHoriz = gridD
+		let gridSquVert = gridD
+		let gridSquGap = 0
+		let grdSquW = (gridW / gridSquHoriz) - (gridSquGap * 2)
+		let grdSquH = (gridH / gridSquVert) - (gridSquGap * 2)
+		this.grid = []
+		for(let i = 0; i < gridSquVert; i++){
+			let row = []
+			for(let j = 0; j < gridSquHoriz; j++){
+				row.push(undefined)
+			}
+			this.grid.push(row)
+		}
+		let rowCount = 0
+		let colCount
+
+		for(let i = gridSquGap; i < gridW; i += grdSquW + (gridSquGap * 2)){
+			colCount = 0
+			for(let j = gridSquGap; j < gridH; j += grdSquH + (gridSquGap * 2)){
+
+				if(separate){
+					let squ = new PIXI.Graphics()
+					squ.beginFill(0xFF3300)
+					squ.drawRect(i, j, grdSquW, grdSquH)
+					app.stage.addChild(squ)
+					this.grid[rowCount][colCount] = squ
+					colCount++
+				}else if(!separate){
+					gridGraphics.drawRect(i, j, grdSquW, grdSquH)
+					this.grid[rowCount][colCount] = [i, j, grdSquW, grdSquH, true]
+					colCount++
+				}
+
+			}
+			rowCount++
+		}
+
+		this.hoverX = -1
+		this.hoverY = -1
+		this.mouseDown = false
+		app.stage.interactive = true
+		app.stage.on('mousemove', (event)=>{
+			this.hoverX = Math.floor(event.data.global.x / (grdSquW + (gridSquGap * 2)))
+			this.hoverY = Math.floor(event.data.global.y / (grdSquH + (gridSquGap * 2)))
+		})
+		window.addEventListener('mousedown', (event)=>{
+			this.mouseDown = true
+		})
+		window.addEventListener('mouseup', (event)=>{
+			this.mouseDown = false
+		})
+		window.addEventListener('mouseupoutside', (event)=>{
+			this.mouseDown = false
+		})
+
+		let callUpdate = ()=> {
+
+			this.update(separate, gridGraphics)
+		}
+		let ticker = PIXI.ticker.shared
+		ticker.add(callUpdate)
+		ticker.start()
+	}
+
+	update(separate, gridGraphics){
+		let hoveredSqu
+
+		if(this.grid[this.hoverY] !== undefined && this.grid[this.hoverY][this.hoverX] !== undefined){
+			if(this.mouseDown){
+				if(separate){
+					hoveredSqu = this.grid[this.hoverX][this.hoverY]
+					hoveredSqu.clear()
+				}else if(!separate){
+					gridGraphics.clear()
+					gridGraphics.beginFill(0xFF3300)
+					hoveredSqu = this.grid[this.hoverX][this.hoverY]
+
+					hoveredSqu[4] = false
+
+					let x = hoveredSqu[0]
+					let y = hoveredSqu[1]
+					let grdSquW = hoveredSqu[2]
+					let grdSquH = hoveredSqu[3]
+
+					for(let i = 0; i < this.grid.length; i++){
+						for(let j = 0; j < this.grid[i].length; j++){
+
+							let sq = this.grid[i][j]
+
+							if(sq[4] == false)
+								continue
+
+							let x = sq[0]
+							let y = sq[1]
+							let grdSquW = sq[2]
+							let grdSquH = sq[3]
+
+							gridGraphics.drawRect(x, y, grdSquW, grdSquH)
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+module.exports = Grid
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let Grid = require('./Grid.js')
+
 document.addEventListener('DOMContentLoaded', function () {
 	let PIXI = require('pixi.js')
 
@@ -16,69 +18,5 @@ document.addEventListener('DOMContentLoaded', function () {
 	})
 	document.body.appendChild(app.view)
 
-	// let gridGraphics = new PIXI.Graphics()
-	// gridGraphics.beginFill(0xFF3300)
-
-	let gridSquHoriz = 50
-	let gridSquVert = 50
-	let gridSquGap = 1
-	let grdSquW = (gridW / gridSquHoriz) - (gridSquGap * 2)
-	let grdSquH = (gridH / gridSquVert) - (gridSquGap * 2)
-	let grid = []
-	for(let i = 0; i < gridSquVert; i++){
-		let row = []
-		for(let j = 0; j < gridSquHoriz; j++){
-			row.push(undefined)
-		}
-		grid.push(row)
-	}
-	let rowCount = 0
-	let colCount
-	for(let i = gridSquGap; i < gridW; i += grdSquW + (gridSquGap * 2)){
-		colCount = 0
-
-		for(let j = gridSquGap; j < gridH; j += grdSquH + (gridSquGap * 2)){
-		
-			let squ = new PIXI.Graphics()
-			squ.beginFill(0xFF3300)
-			squ.drawRect(i, j, grdSquW, grdSquH)
-			app.stage.addChild(squ)
-			// grid[rowCount][colCount] = j + "," + i + "," + (j + grdSquW)
-			grid[rowCount][colCount] = squ
-
-			colCount++
-		}
-
-		rowCount++
-	}
-	console.log(grid)
-
-	// app.stage.addChild(gridGraphics)
-
-	this.hoverX = -1
-	this.hoverY = -1
-	app.stage.interactive = true
-	app.stage.on('mousemove', (event)=>{
-		this.hoverX = Math.floor(event.data.global.x / (grdSquW + (gridSquGap * 2)))
-		this.hoverY = Math.floor(event.data.global.y / (grdSquH + (gridSquGap * 2)))
-	})
-	window.addEventListener('mousedown', (event)=>{
-		console.log("s")
-	})
-
-	let callUpdate = ()=> { 
-		update()
-	}
-	let ticker = PIXI.ticker.shared
-	ticker.add(callUpdate)
-	ticker.start()
-
-	update = ()=>{
-		let hoveredSqu
-		if(grid[this.hoverY] !== undefined && grid[this.hoverY][this.hoverX] !== undefined){
-
-			hoveredSqu = grid[this.hoverX][this.hoverY]
-			hoveredSqu.clear()
-		}
-	}
+	let grids = new Grid(app, gridW, gridH)
 })
